4e45a430d3b18d054726c512f205ce75a4224881
pyguru123 <pyguru123@gmail.com>
2021-02-10 23:46:55 +0530

    updated project

--- Fuel Prices India - Dashboard/app.py
+++ Fuel Prices India - Dashboard/app.py
@@ -6,4 +6,36 @@ import pandas as pd
 from csvparser import get_dataframe
 
 df = get_dataframe()
-print(df)
+
+external_stylesheets = [
+	{
+		"href": "https://fonts.googleapis.com/css2?"
+		"family=Lato:wght@400;700&display=swap",
+		"rel": "stylesheet",
+	},
+]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Fuel Analytics: Understand Fuel Prices!"
+
+app.layout = html.Div(
+	children=[
+		html.Div(
+			children=[
+				html.Img(
+					src="assets/fuel_icon.png", className="header-img"
+				),
+				html.H1(
+					"Fuel Analytics", className="header-title"
+				),
+				html.P(
+					"Analyze the prices of petrol and diesel "
+						"in India between 2001 to 2020.", className="header-description"
+				),
+			], className="header"
+		)
+	]
+)
+
+if __name__ == '__main__':
+	app.run_server(debug=True)
